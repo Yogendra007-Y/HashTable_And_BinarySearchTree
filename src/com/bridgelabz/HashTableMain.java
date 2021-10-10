40b11d4de472a384d6c261bf0805658b255714dc
yogkri <dharmayog91@gmail.com>
2021-10-10 11:54:48 +0530

    Ability To Find Word In Large Paragraph

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -1,9 +1,10 @@
 package com.bridgelabz;
+import java.util.ArrayList;
 
 public class HashTableMain {
     public static void main(String[] args) {
-        MyHashTable<String, Integer> hashTable = new MyHashTable();
-        String sentence = "To be or not to be";
+        MyHashMap<String, Integer> hashTable = new MyHashMap();
+        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
         /* Storing given sentence in an array.
            Converting the sentence to lower case and removing spaces.
@@ -22,115 +23,142 @@ public class HashTableMain {
         }
         System.out.println(hashTable);
     }
-    public static class MyHashTable <K, V> {
-        MyMapNode head;
-        MyMapNode tail;
+}
 
-        /**
-         * Purpose: Method to get the word from Linked List.
-         * @param word: word returned.
-         */
-        public V get(K word) {
-            MyMapNode<K, V> myNode = searchNode(word);
-            return (myNode == null) ? null : myNode.getValue();
-        }
+class MyHashMap <K, V> {
+    MyMapNode<K, V> head;
+    MyMapNode<K, V> tail;
+    private final int numOfBuckets;
+    ArrayList<MyMapNode<K, V>> myBucketArray;
 
-        /**
-         * Purpose: Method to search word from Linked List.
-         * @param word: word to search.
-         */
-        public MyMapNode<K, V> searchNode(K word) {
-            MyMapNode<K, V> currentNode = head;
-            int position = 0;
-            while (currentNode != null) {
-                position++;
-                if (currentNode.getKey().equals(word)) {
-                    return currentNode;
-                }
-                currentNode = currentNode.getNext();
+    public MyHashMap() {
+        this.numOfBuckets = 10;
+        this.myBucketArray = new ArrayList<>(numOfBuckets);
+        // Create empty LinkedLists
+        for (int i = 0; i < numOfBuckets; i++)
+            this.myBucketArray.add(null);
+    }
+
+    /**
+     * Purpose : method to get value from LinkedList using index number
+     * @param key : key is returned
+     */
+
+    public V get(K key) {
+        int index = this.getBucketIndex(key);
+        if (this.myBucketArray.get(index) == null)
+            return null;
+        MyMapNode<K, V> myNode = search(key);
+        return (myNode == null) ? null : myNode.getValue();
+    }
+
+    /**
+     * Purpose : Method to search the word in LinkedList
+     * @param key : key to search
+     */
+
+    public MyMapNode<K, V> search(K key) {
+        MyMapNode<K, V> currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.getKey().equals(key)) {
+                return currentNode;
             }
-            return currentNode;
+            currentNode = currentNode.getNext();
         }
 
-        /**
-         * Purpose: Method to add key and value to hash table.
-         * @param key: word to be added.
-         * @param value: frequency of word.
-         */
-        public void add(K key, V value) {
-            MyMapNode<K, V> myNode = searchNode(key);
-            if (myNode == null) {
-                myNode = new MyMapNode<>(key, value);
-                this.append(myNode);
-            } else
-                myNode.setValue(value);
+        return currentNode;
+    }
 
+    /**
+     * Purpose : Method to add key and value to hash table
+     * @param key : word to be added
+     * @param value: frequency of word
+     */
+    public void add(K key, V value) {
+        int index = this.getBucketIndex(key);
+        MyMapNode<K, V> myNode = this.myBucketArray.get(index);
+        if (myNode == null) {
+            myNode = new MyMapNode<>(key, value);
+            this.myBucketArray.set(index, myNode);
         }
+        myNode = search(key);
+        if (myNode == null) {
+            myNode = new MyMapNode<>(key, value);
+            this.append(myNode);
+        } else
+            myNode.setValue(value);
+    }
 
-        /**
-         * Purpose: Method to append value to Linked List.
-         * @param myNode: value to append.
-         */
+    // This implements hash function to find index for a key
+    public int getBucketIndex(K key) {
+        int hashCode = Math.abs(key.hashCode());
+        int index = hashCode % numOfBuckets;
+        return index;
 
-        private void append(MyMapNode<K, V> myNode) {
-            if (this.head == null)
-                this.head = myNode;
-            if (this.tail == null)
-                this.tail = myNode;
-            else {
-                this.tail.setNext(myNode);
-                this.tail = myNode;
-            }
-        }
+    }
 
-        @Override
-        public String toString() {
-            return "MyLinkedListNodes{" + head + '}';
+    /**
+     * Purpose : Method to append value to Linked List
+     * @param myNode : value to append
+     */
+
+    private void append(MyMapNode<K, V> myNode) {
+        if (this.head == null)
+            this.head = myNode;
+        if (this.tail == null)
+            this.tail = myNode;
+        else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
         }
     }
-    public static class MyMapNode <K,V>{
-        K key;
-        V value;
-        MyMapNode<K, V> next;
 
-        public MyMapNode(K key, V value) {
-            this.key = key;
-            this.value = value;
-            next = null;
-        }
+    @Override
+    public String toString() {
+        return "MyHashMapNodes{" + head + '}';
+    }
+}
+class MyMapNode <K, V> {
+    K key;
+    V value;
+    MyMapNode<K, V> next;
 
-        public K getKey() {
-            return key;
-        }
+    public MyMapNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+        next = null;
+    }
 
-        public void setKey(K key) {
-            this.key = key;
+    public K getKey() {
+        return key;
+    }
 
-        }
+    public void setKey(K key) {
+        this.key = key;
+    }
 
-        public V getValue() {
-            return value;
-        }
+    public V getValue() {
+        return value;
+    }
 
-        public void setValue(V value) {
-            this.value = value;
-        }
+    public void setValue(V value) {
+        this.value = value;
+    }
 
-        public MyMapNode<K, V> getNext() {
-            return next;
-        }
+    public MyMapNode<K, V> getNext() {
+        return next;
+    }
 
-        public void setNext(MyMapNode<K, V> next) {
-            this.next = (MyMapNode<K, V>) next;
-        }
+    public void setNext(MyMapNode<K, V> next) {
+        this.next = next;
+    }
 
-        @Override
-        public String toString() {
-            StringBuilder nodeString = new StringBuilder();
-            nodeString.append("MyMapNode{" + " Key= ").append(key).append(" Value= ").append(value).append('}');
-            if (next != null)
-                nodeString.append("->").append(next);
-            return nodeString.toString();
-        }
+    @Override
+    public String toString() {
+        StringBuilder nodeString = new StringBuilder();
+        nodeString.append("Node{" + "Key=").append(key).append(" Value=").append(value).append("}");
+        if(next != null)
+            nodeString.append("->").append(next);
+        return nodeString.toString();
     }
 }
